eed7045e0df991b49c00c6fe8038f4aaccf222f4
Tim Smith <tim.smith@webtrends.com>
2012-10-15 14:55:23 -0700

    Use 5 character mode def and strings not symbols for the node attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,8 +20,8 @@
 include_recipe "apt"
 
 template "/etc/apt/sources.list" do
-  mode 0644
-  variables :code_name => node[:lsb][:codename]
+  mode 00644
+  variables :code_name => node['lsb']['codename']
   notifies :run, resources(:execute => "apt-get update"), :immediately
   source "sources.list.erb"
 end
