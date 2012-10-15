c0fbafbe0631cae4252c02d961c5a9ed0f2b7a91
Tim Smith <tim.smith@webtrends.com>
2012-10-15 15:20:18 -0700

    Only include source package lines if include_source_packages attribute is true
    This is the default so that existing behavior is maintained

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,8 @@ template "/etc/apt/sources.list" do
   variables(
     :code_name => node['lsb']['codename'],
     :security_url => node['ubuntu']['security_url'],
-    :archive_url => node['ubuntu']['archive_url']
+    :archive_url => node['ubuntu']['archive_url'],
+    :include_source_packages => node['ubuntu'['include_source_packages']
   )
   notifies :run, "execute[apt-get update]", :immediately
   source "sources.list.erb"
