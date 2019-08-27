659915e4cc852a3c8be6789d1fbec9c29948f81e
Tim Smith <tsmith84@gmail.com>
2019-08-27 14:00:08 -0700

    Remove extra metadata from metadata.rb
    These don't do anything
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,8 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Sets up apt sources for Ubuntu Linux'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.1'
 supports 'ubuntu'
-recipe 'ubuntu', 'Sets up sources for the nodes Ubuntu release'
 
 source_url 'https://github.com/chef-cookbooks/ubuntu'
 issues_url 'https://github.com/chef-cookbooks/ubuntu/issues'
